7f7fcde6ca03443fbdc3bf81f89b87795bb1c83f
tiansibatuara <sibatuarachristian@gmail.com>
2024-03-14 23:19:28 +0700

    redirect 5s

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,32 @@
+"use client";
+
 import Image from "next/image";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const [lineInfo, setLineInfo] = useState(null);
+
+  useEffect(() => {
+    const eventSource = new EventSource(
+      "https://staging-poc-api.antrein.com/events"
+    );
+    eventSource.onmessage = (event) => {
+      console.log(event);
+      const newData = JSON.parse(event.data);
+      console.log(newData);
+      setLineInfo(newData);
+    };
+
+    const redirectTimeout = setTimeout(() => {
+      window.location.href = "https://ticket.antrein.com/";
+    }, 5000);
+
+    return () => {
+      eventSource.close();
+      clearTimeout(redirectTimeout); // Clear the timeout to prevent memory leaks
+    };
+  }, []);
+
   return (
     <div
       className="hero min-h-screen "
@@ -10,32 +36,6 @@ export default function Home() {
       }}
     >
       <div className="hero-overlay bg-opacity-60"></div>
-
-      {/* <div className="card w-[32%] glass">
-        <figure>
-          <img
-            src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
-            alt="car!"
-          />
-        </figure>
-        <div className="card-body flex-col items-center">
-          <h2 className="card-title mb-5 text-4xl font-bold text-white">YOU ARE NOW IN LINE</h2>
-          <p className="mb-5 text-white">
-            You are in line for TEST EVENT. When it is your turn, you will have
-            10 minutes to finish the order.
-          </p>
-          <div>
-            <progress className="progress w-56" value={50} max="100"></progress>
-            <progress className="progress w-56"></progress>
-          </div>
-
-          <p className="my-5 text-white">You number in line: </p>
-          <p className="text-white">You estimated wait time: </p>
-          <div className="card-actions justify-end">
-            <button className="btn btn-primary">Learn now!</button>
-          </div>
-        </div>
-      </div> */}
 
       <div className="hero-content text-center text-neutral-content bg-[rgba(100,100,100,0.5)] px-[80px] backdrop-blur rounded-xl py-8 w-[32%]">
         <div className="">
@@ -54,6 +54,8 @@ export default function Home() {
 
           <p className="mt-5 text-white">You number in line: </p>
           <p className="text-white">You estimated wait time: </p>
+
+          <button className="btn btn-primary mt-5">Redirect</button>
         </div>
       </div>
     </div>
